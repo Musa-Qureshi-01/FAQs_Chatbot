292ae3fce33ad37a4f0aae3b97d0440e394e5f80
Musa-Qureshi-01 <musaqureshi788code@gmail.com>
2025-08-21 15:30:01 +0530

    Initial commit - FAQ Bot

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from helper import vector_db, get_chain
+
+st.title("FAQ's Bot 🌱")
+
+st.subheader("📂 Knowledge Base")
+btn = st.button("Create KnowledgeBase")
+if btn:
+    st.success("KnowledgeBase created successfully!")
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_chain()
+    result = chain(question)
+    st.subheader("Answer")
+    st.write(result['result'])
+    st.subheader("Internal Model Work.")
+    st.write(result)
+    # btn = st.button("Enter")
+    # if btn:
+    #     st.subheader("Answer")
+    #     st.write(result['result'])
+    #     st.subheader("Internal Model Work.")
+    #     st.write(result)
+
